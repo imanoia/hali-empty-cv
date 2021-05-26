d0e215a6abf16f8385aad57140f7b70e142550a4
imanoia <30325160+imanoia@users.noreply.github.com>
2021-05-27 00:44:09 +0300

    Add files via upload

---
+++ canvas/canvas.js
@@ -0,0 +1,15 @@
+var mycanvas = document.getElementById('c'),
+    
+    mycontext = mycanvas.getContext('2d');
+
+mycontext.fillStyle = '#39d';
+
+mycontext.fillRect(0, 0, 100, 50);
+
+mycontext.fillStyle = '#2d9';
+
+mycontext.fillRect(10, 10, 100, 50);
+
+mycontext.fillStyle = '#d98';
+
+mycontext.fillRect(20, 20, 100, 50);
